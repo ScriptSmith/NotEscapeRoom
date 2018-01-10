4b7939d137d66a74c8aafea0dfb35885610d5776
ScriptSmith <ScriptSmith@users.noreply.github.com>
2018-01-10 11:45:23 +1000

    desktop mobile

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import *
 from flask_sqlalchemy import SQLAlchemy
 import random
+import string
 
 app = Flask(__name__)
 app.config.from_json('config.json')
@@ -13,6 +14,7 @@ class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     hub = db.Column(db.Integer, db.ForeignKey('hubs.id'))
     clients = db.relationship("Client", backref="clients")
+    code = db.Column(db.String)
     complete = db.Column(db.Boolean)
 
 
@@ -41,50 +43,41 @@ def create_tables():
 
 @app.route('/')
 def root():
-    name = request.args.get('name')
-    name = 'Player' if not name else name
-    started = request.args.get('started')
-    return render_template("index.html", name=name, started=started)
+    if request.user_agent.platform in ['android', 'ios']:
+        return render_template("mobile.html")
+    else:
+        return desktop()
+
+
+def desktop():
+    hub = create_hub()
+    game = create_game(hub)
+    db.session.commit()
+    return render_template("desktop.html", game=game, hub=hub)
+
+
+def create_hub():
+    hub = Hub(key=generate_key(5), progress='.....')
+    db.session.add(hub)
+    return hub
+
+
+def create_game(hub):
+    game = Game(hub=hub.id, code=generate_join_code(), complete=False)
+    db.session.add(game)
+    return game
+
+
+def generate_join_code():
+    code = ''.join(random.choices(string.ascii_uppercase, k=5))
+    for game in Game.query.all():
+        if game.code == code:
+            return generate_join_code()
+    return code
 
 
 def generate_key(length):
     return ''.join(random.choices("↖↗↘↙←↓↑→", k=length))
-
-
-@app.route('/create_game')
-def create_game():
-    hub = Hub(key=generate_key(6), progress='......')
-    db.session.add(hub)
-    game = Game(hub=hub.id, complete=False)
-
-    db.session.add(game)
-    db.session.commit()
-
-    response = {
-        'game': game.id,
-        'hub': hub.id,
-        'key': hub.key
-    }
-
-    return jsonify(response)
-
-
-@app.route('/join_game')
-def join_game():
-    game_id = request.args.get('game')
-    game = Game.query.filter_by(id=game_id).first()
-
-    response = {
-        'message': 'failed',
-    }
-
-    if game:
-        client = Client(game=game_id, score=0)
-        db.session.add(client)
-        response['client'] = client.id
-        response['message'] = 'succeeded'
-
-    return jsonify(response)
 
 
 if __name__ == '__main__':
